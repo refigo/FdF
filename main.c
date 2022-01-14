47ce08caa4fb90e7707f9a38a73ac94e996a3d21
Refi Go <64242999+refigo@users.noreply.github.com>
2022-01-14 16:55:21 +0900

    laptop fdf test

--- main.c
+++ main.c
@@ -5,3 +5,20 @@ int	main(void)
 	// todo: handling minilibX
 	return (0);
 }
+
+/*
+- allowed functions(mandatory)
+
+open, read, write, close
+malloc, free
+perror, strerror
+exit
+
+math library
+miniLibX library
+
+libft
+get_next_line
+ft_split
+ft_getnbr
+*/
